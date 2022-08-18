841068ab7780a340987c8118ac00ab7b96cc7a77
Carlos Augusto <junior.rg27@gmail.com>
2022-08-18 14:47:42 -0300

    Primeiro Contato com API

---
+++ BookApi/Model/BookContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace BooksApi.Model
+{
+    public class BookContext : DbContext
+    {
+        public BookContext(DbContextOptions options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ BookApi/Repositories/IBookRepository.cs
@@ -0,0 +1,23 @@
+﻿using BooksApi.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+namespace BooksApi.Repositories
+{
+    public interface IBookRepository
+    {
+        Task<IEnumerable<Book>> Get();
+
+        Task<Book> Get(int Id);
+
+        Task<Book> Create (Book book);
+
+        Task Update (Book book);
+
+        Task Delete (int Id);
+    }
+}
